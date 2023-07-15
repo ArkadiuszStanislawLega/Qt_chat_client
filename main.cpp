5628f28663e76466c5106343ce14688b80fcb260
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2023-07-15 16:42:21 +0200

    Added basic classes

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include "client.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Client client;
+    client.connectToHost("127.0.0.1", 2020);
+    return a.exec();
+}
