1e226fb3ee75a0b5ebd1aa96c42926a3959d21d2
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2023-07-15 16:46:02 +0200

    Update main - code review constant values added.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <QCoreApplication>
 #include "client.h"
 
+#define PORT 2020
+#define IP "127.0.0.1"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     Client client;
-    client.connectToHost("127.0.0.1", 2020);
+    client.connectToHost(IP, PORT);
     return a.exec();
 }
